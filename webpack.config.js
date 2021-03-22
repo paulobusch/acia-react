8f8e770e8f016e4a6b1238cc48d246b131cf8769
Paulo Ricardo Busch <paulo202015@outlook.com.br>
2021-03-22 09:38:21 -0300

    Change output

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
   entry: './src/Index.jsx',
   output: {
-    path: __dirname + '/dist',
+    path: __dirname + '/public',
     filename: './app.js'
   },
   devServer: {
