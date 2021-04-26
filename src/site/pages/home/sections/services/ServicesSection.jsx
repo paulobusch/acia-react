48c330423f29486569763c84bfdb15879a89362b
Paulo Ricardo Busch <paulo202015@outlook.com.br>
2021-04-25 22:57:52 -0300

    feat: service links

--- src/site/pages/home/sections/services/ServicesSection.jsx
+++ src/site/pages/home/sections/services/ServicesSection.jsx
@@ -7,22 +7,22 @@ import ServiceCard from './service-card/ServiceCard';
 
 export default props => (
   <Section id="services">
-    <ServiceCard icon="credit-card" title="ACIA CRED">
+    <ServiceCard icon="credit-card" title="ACIA CRED" href="https://www.aciaanapolis.com.br/site/aciacred-convenio/">
       CARTÃO DE CRÉDITO DO
       TRABALHADOR, BOM PARA
       A EMPRES BOM PARA O FUNCIONÁRIO
     </ServiceCard>
-    <ServiceCard icon="desktop" title="CERTIFICADO DIGITAL">
+    <ServiceCard icon="desktop" title="CERTIFICADO DIGITAL" href="https://www.aciaanapolis.com.br/site/certificado-digital-acia/">
       O CERTIFICADO DIGITAL DA ACIA É UMA
       EXCELENTE OPÇÃO PARA QUEM QUER,
       USUFRUIR DE SEGURANÇA E BAIXO CUSTO
     </ServiceCard>
-    <ServiceCard icon="search" title="SERASA EXPERIAN">
+    <ServiceCard icon="search" title="SERASA EXPERIAN" href="https://www.aciaanapolis.com.br/site/serasa-experian/">
       OBTENHA INFORMAÇÕES COMERCIAIS E
       FINANCEIRAS SOBRE PESSOAS E EMPRESAS,
       DE FORMA SIMPLES E ONLINE.
     </ServiceCard>
-    <ServiceCard icon="mobile-alt" title="REDE CELULAR">
+    <ServiceCard icon="mobile-alt" title="REDE CELULAR" href="https://www.aciaanapolis.com.br/site/rede-celular/">
       ECONOMIA COM QUALIDADE É COM A REDE
       CELULAR, ONDE OFERECEMOS PLANOS DE
       LIGAÇÃO COM ECONOMIA PARA VOCÊ.
--- src/site/pages/home/sections/services/service-card/ServiceCard.jsx
+++ src/site/pages/home/sections/services/service-card/ServiceCard.jsx
@@ -1,12 +1,14 @@
 import './ServiceCard.css';
 
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router';        
 
-export default props => (
-  <Link to={ props.to } className="service-card">
+export default function ServiceCard(props) {
+  const service = <div>
     <i className={ `fas fa-${props.icon}` }></i>
     <h2>{ props.title }</h2>
     <p>{ props.children }</p>
-  </Link>
-);
+  </div>;
+  if (props.href) return (<a href={ props.href } className="service-card">{ service }</a>);
+  return (<Link to={ props.to } className="service-card">{ service }</Link>);
+}
